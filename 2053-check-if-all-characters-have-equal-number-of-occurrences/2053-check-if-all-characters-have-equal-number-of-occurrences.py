009f1bf0cca58b3321595b57a8dd17e9c23adfa7
Sydronee <127466385+Sydronee@users.noreply.github.com>
2025-08-26 20:59:25 +0530

    2053-CHECK-IF-ALL-CHARACTERS-HAVE-EQUAL-NUMBER-OF-OCCURRENCES Stats: Time: 0 ms (100%), Space: 17.7 MB (71.15%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,13 +1,5 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        occur={}
-        for i in s:
-            if i not in occur.keys():
-                occur[i]=1
-            else:
-                occur[i]+=1
-        test=occur[s[0]]
-        for i in occur.values():
-            if i != test:
-                return False
-        return True
+        counts = Counter(s).values()
+        distinct = set(counts)
+        return len(distinct) == 1
