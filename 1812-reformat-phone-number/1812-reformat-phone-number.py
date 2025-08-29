1adc4147ae95f9761774fdca0712b8600bf99798
Sydronee <127466385+Sydronee@users.noreply.github.com>
2025-08-29 20:19:08 +0530

    1812-REFORMAT-PHONE-NUMBER Stats: Time: 0 ms (100%), Space: 17.7 MB (59.3%) - LeetHub

--- 1812-reformat-phone-number/1812-reformat-phone-number.py
+++ 1812-reformat-phone-number/1812-reformat-phone-number.py
@@ -1,16 +1,17 @@
 class Solution:
     def reformatNumber(self, number: str) -> str:
-        number=''.join(number.split())
-        number=''.join(number.split('-'))
-        n=len(number)
+        number = "".join(number.split())
+        number = "".join(number.split("-"))
 
-        m=[number[i:i+3] for i in range(0, n, 3)]
-        if n%3==1:
-            res="-".join(m[:(n//3)-1])
-            s=m[n//3-1:]       
-            s=list(''.join(s))
-            s.insert(2,"-")
-            s="".join(s)        
+        n = len(number)
+        m = [number[i : i + 3] for i in range(0, n, 3)]
+
+        if n % 3 == 1:
+            res = "-".join(m[: (n // 3) - 1])
+            s = m[n // 3 - 1 :]
+            s = list("".join(s))
+            s.insert(2, "-")
+            s = "".join(s)
 
             if res:
                 res += "-"
