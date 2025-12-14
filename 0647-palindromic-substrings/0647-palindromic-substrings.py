5b50cdca755f38983c69cd96db9b8dd73b977907
Sydronee <127466385+Sydronee@users.noreply.github.com>
2025-12-14 16:11:02 +0530

    0647-PALINDROMIC-SUBSTRINGS Stats: Time: 128 ms (62.84%), Space: 17.7 MB (65.5%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,17 +3,15 @@ class Solution:
         count=0
 
         def expand(left, right):
-            localcount=0
-
+            curCount=0
             while left>=0 and right<len(s) and s[left]==s[right]:
-                localcount+=1
+                curCount+=1
                 left-=1
                 right+=1
-
-            return localcount
+            return curCount
 
         for i in range(len(s)):
-            count+=expand(i,i)
+            count+=expand(i,i) 
             count+=expand(i,i+1)
 
-        return count 
+        return count
